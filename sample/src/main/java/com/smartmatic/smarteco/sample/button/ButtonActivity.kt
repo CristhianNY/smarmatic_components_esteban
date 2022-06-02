bc682269d0bbbb0fac696ae81c8a908ea0f62dab
Crishian Bonilla <cristhian.bonilla@smartmatic.com>
2022-06-02 18:49:32 -0500

    Optimize imports by Android Studio

--- sample/src/main/java/com/smartmatic/smarteco/sample/button/ButtonActivity.kt
+++ sample/src/main/java/com/smartmatic/smarteco/sample/button/ButtonActivity.kt
@@ -1,7 +1,6 @@
 package com.smartmatic.smarteco.sample.button
 
 import android.os.Bundle
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.smartmatic.smarteco.sample.R
 
